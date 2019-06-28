2ae4a85d71cc7f9dce21747c40ce7c2f5fbe62f5
Karel-van-de-Plassche <karelvandeplassche@gmail.com>
2019-06-28 16:36:23 +0200

    WIP: Messy merge of example, AbstractSlider and bokeh-1.0.5

--- bokeh-ion-rangesliderjs/src/ion_range_slider.ts
+++ bokeh-ion-rangesliderjs/src/ion_range_slider.ts
@@ -1,26 +1,69 @@
-//import $ = require('jquery')
-//import {IonRangeSliderOptions, IonRangeSliderEvent} from "ion-rangeslider"
+//import {throttle} from "core/util/callback"
 
-import {throttle} from "core/util/callback"
 // The "core/properties" module has all the property types
 import * as p from "core/properties"
-import {div, input, label} from "core/dom"
-import {logger} from "core/logging"
+
+// HTML construction and manipulation functions
+import {div, input} from "core/dom"
+
+import {SliderCallbackPolicy} from "core/enums"
 
 // We will subclass in JavaScript from the same class that was subclassed
 // from in Python
-import {Widget} from "models/widgets/widget"
 import {AbstractSlider, AbstractSliderView, SliderSpec} from "models/widgets/abstract_slider"
+import {ControlView} from "models/widgets/control"
+import {throttle} from "core/util/callback"
+throttle // Stop TS from complaining
 
+declare function jQuery(...args: any[]): any
+
+const prefix = 'bk-ionRange-'
+prefix // Stop TS from complaining
+
+export interface StringSliderSpec {
+  start: number
+  end: number
+  value: number[]
+  step: number
+}
 
 export class IonRangeSliderView extends AbstractSliderView {
   model: IonRangeSlider
 
-  protected sliderEl: HTMLElement
-  protected titleEl: HTMLElement
-  protected valueEl: HTMLElement
-  protected value_type: string
-  protected callback_wrapper?: () => void
+  protected value_el?: HTMLInputElement
+
+  connect_signals(): void {
+    // Workaround to call the grandparent connect_signals
+    ControlView.prototype.connect_signals.call(this)
+
+    const {callback, callback_policy, callback_throttle} = this.model.properties
+    this.on_change([callback, callback_policy, callback_throttle], () => this._init_callback())
+
+    //const {start, end, value, step, title} = this.model.properties
+    //this.on_change([start, end, value, step], () => {
+    //  const {start, end, value, step} = this._calc_to()
+    //  this.noUiSlider.updateOptions({
+    //    range: {min: start, max: end},
+    //    start: value,
+    //    step,
+    //  })
+    //})
+
+    const {bar_color} = this.model.properties
+    this.on_change(bar_color, () => {
+      this._set_bar_color()
+    })
+
+    //this.on_change([value, title], () => this._update_title())
+  }
+
+  _update_title(): void {
+    console.error('_update_title should never be called by ionRangeSlider!')
+  }
+
+  protected _set_bar_color(): void {
+    console.error('_set_bar_color should never be called by ionRangeSlider!')
+  }
 
   protected _calc_to(): SliderSpec {
     return {
@@ -31,71 +74,49 @@ export class IonRangeSliderView extends AbstractSliderView {
     }
   }
 
-  protected _calc_from(values: string[] | string): string[] | number[] {
-    var parsed_values: string[] | number[]
+  protected _calc_from(values: number[]): number | number[] {
+    console.error('_calc_from should never be called by ionRangeSlider!')
+    values // Stop TS from complaining
+    return NaN
+  }
 
-    if (this.model.slider_type === 'double') {
-        parsed_values = values.split(';')
+  protected _calc_from_ion(data: any): number | number[] | string | string[] {
+    // Extract slider positions from ionRangeSlider data structure.
+    // If 'values' was given and was an array of strings, these are strings
+    // If 'values' was not given or an array of numbers, these are numbers
+    console.log(typeof data.from_value)
+    if (data.to_value == null) {
+      // single slider
+      return data.from_value
     } else {
-        if (!(values instanceof Array)) {
-          parsed_values = new Array(values, values)
-        } else {
-          parsed_values = values
-        }
+      // range slider
+      return [data.from_value, data.to_value]
     }
-
-    if (parsed_values.every($.isNumeric)) {
-        parsed_values = parsed_values.map(Number)
-    }
-
-    return parsed_values
   }
 
-  initialize(options: any): void {
-    super.initialize(options)
-    this.render()
+  protected _set_keypress_handles(): void {
+    console.error('_set_keypress_handles should never be called by ionRangeSlider!')
   }
+
+  protected _keypress_handle(e: KeyboardEvent, idx: 0 | 1 = 0): void {
+    console.error('_keypress_handle should never be called by ionRangeSlider!')
+    e // Stop TS from complaining
+    idx // Stop TS from complaining
+  }
+
 
   render(): void {
-    if (this.model.format) {
-      console.warn('[ionRangeSlider] Option format currently ignored')
-    }
-    if (this.sliderEl == null) {
-      // XXX: temporary workaround for _render_css()
-      this._render_classes() // XXX: because no super()
+    // BokehJS Views create <div> elements by default, accessible as @el.
+    // Many Bokeh views ignore this default <div>, and instead do things
+    // like draw to the HTML canvas. In this case though, we change the
+    // contents of the <div>, based on the current slider value.
 
-      // LayoutDOMView sets up lots of helpful things, but
-      // it's render method is not suitable for widgets - who
-      // should provide their own.
-      if (this.model.height != null)
-        this.el.style.height = `${this.model.height}px`
-      if (this.model.width != null)
-        this.el.style.width = `${this.model.width}px`
-    }
+    // Workaround to call the grandparent render
+    ControlView.prototype.render.call(this)
 
-    if (this.model.callback != null) {
-      const callback = () => this.model.callback.execute(this.model)
-
-      switch (this.model.callback_policy) {
-        case 'continuous': {
-          this.callback_wrapper = callback
-          break
-        }
-        case 'throttle': {
-          this.callback_wrapper = throttle(callback, this.model.callback_throttle)
-          break
-        }
-        default: {
-            console.warn('[ionRangeSlider] Unexpected callback_policy')
-            this.callback_wrapper = callback
-        }
-      }
-    }
-    //
-    // Set up parameters
     const {start, end, value, step} = this._calc_to()
 
-    //let tooltips: boolean | any[] // XXX
+    // XXX: tooltips not implemented/applicable
     //if (this.model.tooltips) {
     //  const formatter = {
     //    to: (value: number): string => this.model.pretty(value),
@@ -104,88 +125,62 @@ export class IonRangeSliderView extends AbstractSliderView {
     //  tooltips = repeat(formatter, value.length)
     //} else
     //  tooltips = false
-
-    this.el.classList.add("bk-slider")
-    if (this.sliderEl == null) {
-      this.sliderEl = input({type: "text", class: "slider", id: this.model.id})
-      //this.sliderEl = input({type: "text", class: "slider", id: 'blerger'})
-      this.el.appendChild(this.sliderEl) // XXX: bad typings; no cssPrefix
-
-
-      var opts: IonRangeSliderOptions = {
+    //const [from, to] = this.model.range || [max, min]
+    const from = value[0]
+    const to = value[1]
+    if (this.slider_el == null) {
+      // Slider does not exists, initialize
+      this.slider_el = input({type: "text"}) as any
+      this.el.appendChild(div({style: {width: "100%"}}, this.slider_el))
+      // Set up IonRangeSlider option based on
+      const opts = {
         type: this.model.slider_type,
-      }
-      if (this.model.values instanceof Array) {
-        // If 'this.model.values' is provided, 'from' and 'to' are the indexes in the 'values' array.
-        opts.values = this.model.values
-        opts.from = opts.values.findIndex(val => val == value[0])
-        opts.to = opts.values.findIndex(val => val == value[1])
-      } else {
-        // For a normal slider, from and to are the actual value of the slider
+      } as any
+      if (this.model.values == null) {
+        // If values is not given, initialiaze slider from min, max and step
         opts.min  = start
         opts.max  = end
         opts.step = step
         opts.from = value[0]
         opts.to   = value[1]
+      } else {
+        // If values is given min, max and step are ignored.
+        opts.values = this.model.values
+        opts.from = opts.values.findIndex((val: string | number) => val == value[0])
+        opts.to = opts.values.findIndex((val: string | number) => val == value[1])
       }
-      this.value_type = typeof value[0]
-      opts.grid = this.model.grid
-      opts.prettify_enabled = this.model.prettify_enabled
-      opts.onChange = (_, __, values) => this._slide(values)
-      opts.onFinish = (_, __, values) => this._change(values)
+      opts.prettify_enabled = this.model.prettify_enabled // Enable ionRanges prettify functionality
       if (this.model.prettify)
-        opts.prettify = (f) => {return this._prettify(f) }
-      opts.force_edges = this.model.force_edges
-      opts.prefix = this.model.prefix
-      opts.disable = this.model.disabled
+        // Enable user supplied JS function
+        opts.prettify = (f: any) => {return this._prettify(f) }
+      opts.grid = this.model.grid // Enables grid of values under the slider
 
-      $(this.sliderEl).ionRangeSlider(opts);
+      opts.onChange = (values: any) => this._slide(values) // Triggers on values change
+      //opts.onChange = function (data: any) {
+      //      console.dir(data);
+      //  }
+      //opts.onFinish = (_, __, values) => this._change(values) // Triggers when user releases handle
 
-      $(this.el).find('.irs-bar').css('background', this.model.bar_color)
-      $(this.el).find('.irs-bar-edge').css('background', this.model.bar_color)
-      $(this.el).find('.irs-single').css('background', this.model.bar_color)
+      opts.force_edges = this.model.force_edges // Force ionRangeSlider to be inside its container
+      opts.prefix = this.model.prefix // Prepend on handle before the value displayed
+      opts.disable = this.model.disabled // Disable slider
 
-      if (this.callback_wrapper != null)
-          this.callback_wrapper()
-
-
-      if (this.titleEl != null)
-        this.el.removeChild(this.titleEl)
-      if (this.valueEl != null)
-        this.el.removeChild(this.valueEl)
-
-      if (this.model.title != null) {
-        if (this.model.title.length != 0) {
-          this.titleEl = label({}, `${this.model.title}:`)
-          this.el.insertBefore(this.titleEl, this.sliderEl)
-        }
-
-        if (this.model.show_value) {
-          const pretty = value.map((v) => this.model.pretty(v)).join(" .. ")
-          this.valueEl = div({class: "bk-slider-value"}, pretty)
-          this.el.insertBefore(this.valueEl, this.sliderEl)
-        }
-      } //endif: this.model.title != null
-    } //endif: this.sliderEl == null
-  } //function: render
-
-  protected _prettify(data): void {
-    if (this.model)
-      if (this.model.prettify)
-        return this.model.prettify.execute(data)
-  }
-  protected _slide(data): void {
-    const ion_value: string[] = $(this.sliderEl).prop('value')
-    const value: number[] | string[] = this._calc_from(ion_value)
-    this.model.value = value
-      if (this.callback_wrapper != null)
-        this.callback_wrapper()
+      //this.value_type = typeof value[0] // Save if the value is a number or a string
+      jQuery(this.slider_el).ionRangeSlider(opts)
+      if (this.value_el != null)
+        this.value_el.value = `${from} - ${to}`
+    }
   }
 
-  protected _change(data): void {
-    const ion_value: string[] = $(this.sliderEl).prop('value')
-    const value: number[] | string[] = this._calc_from(ion_value)
-    this.model.value = value
+  protected _slide(data: any): void {
+    this.model.value = this._calc_from_ion(data)
+    if (this.callback_wrapper != null)
+      this.callback_wrapper()
+  }
+
+  protected _change(data: any): void {
+    this.model.value = this._calc_from_ion(data)
+    this.model.value_throttled = this.model.value
     switch (this.model.callback_policy) {
       case 'mouseup':
       case 'throttle': {
@@ -195,50 +190,36 @@ export class IonRangeSliderView extends AbstractSliderView {
       }
     }
   }
-} //namespace: IonRangeSliderView
-
-      //    opts = {
-      //      onChange: this.slide,
-      //      onFinish: this.slidestop,
-      //    }
-      //
-      //    this.model.range = range
-      //    this.$el.find( "////{ this.model.id }" ).val( range.join(' - '))
-      //    this.$el.find('.bk-slider-parent').height(this.model.height)
-      //
-      //
-      //  slidestop: (data) =>
-      //    if this.model.callback_policy == 'mouseup' or this.model.callback_policy == 'throttle'
-      //      this.model.callback?.execute(this.model)
-      //
-      //  slide: (data) =>
-      //    range = [data.from, data.to]
-      //    value = range.join(' - ')
-      //    this.$el.find( "////{ this.model.id }" ).val( value )
-      //    this.model.range = range
-      //    if this.callbackWrapper then this.callbackWrapper()
-      //
-      //  prettify: (data) =>
-      //    this.model.prettify?.execute(data)
+  protected _prettify(data: any): void {
+    if (this.model)
+      if (this.model.prettify)
+        return this.model.prettify.execute(data)
+  }
+}
 
 export namespace IonRangeSlider {
-  export interface Attrs extends Widget.Attrs {
-    slider_type:       string
-    values:            any
-    grid:              boolean
-    prettify_enabled:  boolean
-    prettify:          any
-    force_edges:       boolean
-    prefix:            string
-  }
+  export type Attrs = p.AttrsOf<Props>
 
-  export interface Props extends Widget.Props {}
+  export type Props = AbstractSlider.Props & {
+    slider_type: p.Property<string>
+    values: p.Property<string|string[]|number|number[]>
+    //range: p.Property<[number, number]>
+    start: p.Property<number>
+    end: p.Property<number>
+    step: p.Property<number>
+    grid: p.Property<boolean>
+    prettify_enabled: p.Property<boolean>
+    prettify: p.Property<any>
+    force_edges: p.Property<boolean>
+    prefix: p.Property<string>
+    callback_throttle: p.Property<number>
+    callback_policy: p.Property<SliderCallbackPolicy>
+  }
 }
 
 export interface IonRangeSlider extends IonRangeSlider.Attrs {}
 
 export class IonRangeSlider extends AbstractSlider {
-
   properties: IonRangeSlider.Props
 
   constructor(attrs?: Partial<IonRangeSlider.Attrs>) {
@@ -246,37 +227,44 @@ export class IonRangeSlider extends AbstractSlider {
   }
 
   static initClass(): void {
+    // The ``type`` class attribute should generally match exactly the name
+    // of the corresponding Python class.
     this.prototype.type = "IonRangeSlider"
+
+    // If there is an associated view, this is boilerplate.
     this.prototype.default_view = IonRangeSliderView
 
-    this.define({
+    // The @define block adds corresponding "properties" to the JS model. These
+    // should basically line up 1-1 with the Python model class. Most property
+    // types have counterparts, e.g. bokeh.core.properties.String will be
+    // p.String in the JS implementation. Where the JS type system is not yet
+    // as rich, you can use p.Any as a "wildcard" property type.
+    this.define<IonRangeSlider.Props>({
       slider_type:       [ p.String,      "single"     ],
       values:            [ p.Any,                      ],
-      grid:              [ p.Bool,        true         ],
-      prettify_enabled:  [ p.Bool,        true         ],
+      //range:             [ p.Any                              ],
+      //start:             [ p.Number,               0          ],
+      //end:               [ p.Number,               1          ],
+      //step:              [ p.Number,               0.1        ],
+      grid:              [ p.Boolean,              true       ],
+      prettify_enabled:  [ p.Boolean,        true         ],
       prettify:          [ p.Any,         null         ],
-      force_edges:       [ p.Bool,        false        ],
+      force_edges:       [ p.Boolean,        false        ],
       prefix:            [ p.String,      ""           ],
+      //callback_throttle: [ p.Number,               200        ],
+      //callback_policy:   [ p.SliderCallbackPolicy, "throttle" ],
     })
 
     this.override({
       bar_color: '#ed5565',
       start: 0,
       end: 1,
-      value: [0, 1],
+      value: ['fish', 'pie'],
       step: 0.1,
       show_value: false,
-      title: ''
+      title: '',
+      callback_policy: 'continuous'
     })
   }
-
-  protected _formatter(value: number, _format: string): string {
-    return `${value}`
-  }
-
-  pretty(value: number): string {
-    return this._formatter(value, this.format)
-  }
 }
-
 IonRangeSlider.initClass()
